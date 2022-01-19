b331b6da3f2c9200c9aca9dc44ca560b960876dc
Kenny Tat <kennytat.app@gmail.com>
2022-01-19 15:35:55 +0700

    edit index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,77 +7,89 @@ import M3U8FileParser from 'm3u8-file-parser';
 // import { slice } from 'ramda';
 // import { create, globSource, CID } from 'ipfs-http-client'
 // edit info here
-const startPoint = 574; 
-const endPoint = 575;
-const fileType = 'audio' // 'audio';
-const quality = '128';
-const VGM = 'VGMA'; // 'VGMA'
-const prefix = execSync('pwd', {encoding: 'utf8'}).replace('\n',''); // '/home/vgm/Desktop'; // .
-const queue = new PQueue({ concurrency: 1 });
+const startPoint = 0; // start at 5652 next 
+const endPoint = 1;
+const fileType = 'video' // 'audio';
+const quality = '480';
+const VGM = 'VGMV'; // 'VGMA'
+const prefix = '/home/vgm/Desktop'; // '/home/vgm/Desktop'; // execSync('pwd', {encoding: 'utf8'}).replace('\n',''); 
+const queue = new PQueue({ concurrency: 2 });
 const txtPath = `${prefix}/database/${VGM}Single.txt`;
 const convertedPath = `VGM-Converted:vgmencrypted/encrypted/${VGM}`;
-const gateway = `https://cdn.vgm.tv/encrypted/${VGM}`;
-const localTemp = `${prefix}/database/tmp`; // `/mnt/6TBHDD/VGMDATA/${VGM}`  // `${prefix}/database/tmp`
+// const gateway = `https://cdn.vgm.tv/encrypted/${VGM}`;
+const localTemp = `${prefix}/database/tmp` ; // `/mnt/6TBHDD/VGMDATA/${VGM}`  // `${prefix}/database/tmp`
 // const originalTemp = `${prefix}/database/tmp`;
 // const apiPath = `${prefix}/database/API/items/single`;
 // const ipfsClient = create({ host: 'localhost', port: 9095 }) // http://ipfs-cluster-stackos.hjm.bid/ port: 80 localhost port:9095
 // edit info end
 
-const checkFileExists = async (fileUrl) => {
-      return new Promise((resolve) => {
-        const url = `${gateway}/${fileUrl}/${quality}p.m3u8`; // if video 480p.m3u8 audio 128p.m3u8
-        console.log('checking url:', url);
+// const checkFileExists = async (fileUrl) => {
+//       return new Promise((resolve) => {
+//         const url = `${gateway}/${fileUrl}/${quality}p.m3u8`; // if video 480p.m3u8 audio 128p.m3u8
+//         console.log('checking url:', url);
 
-        // // check thumb url
-        exec(`curl --silent --head --fail ${url}`, async (error, stdout, stderr) => {
-          if (error) {
-            console.log('file exist:', false);
-            await fs.appendFileSync(`${prefix}/database/${fileType}-ipfs-count.txt`, `\n${url} --fileMissing`);
-            resolve(false)
-          };
-          if (stderr) console.log('stderr', stderr);
-          if (stdout) {
-            console.log('file exist:', true);
-            // await fs.appendFileSync(`${prefix}/database/${fileType}-converted-count.txt`, `\n${url} --fileExist`);
-            resolve(true);
-          };
-        });
-      });
-    }
+//         // // check thumb url
+//         exec(`curl --silent --head --fail ${url}`, async (error, stdout, stderr) => {
+//           if (error) {
+//             console.log('file exist:', false);
+//             await fs.appendFileSync(`${prefix}/database/${fileType}-inipfs-count.txt`, `\n${url} --fileMissing`);
+//             resolve(false)
+//           };
+//           if (stderr) console.log('stderr', stderr);
+//           if (stdout) {
+//             console.log('file exist:', true);
+//             // await fs.appendFileSync(`${prefix}/database/${fileType}-converted-count.txt`, `\n${url} --fileExist`);
+//             resolve(true);
+//           };
+//         });
+//       });
+//     }
+
+// const checkFileIsFull = async (outPath) => {
+//    return new Promise(async (resolve) => { 
+//     const keyPath = `${outPath}/key.vgmk`;
+//     const m3u8Path = `${outPath}/128p.m3u8`;
+//     if (fs.existsSync(outPath) && fs.existsSync(keyPath) && fs.existsSync(m3u8Path)) {
+//       const reader = new M3U8FileParser();
+//       const segment = await fs.readFileSync(m3u8Path, { encoding: 'utf-8' });
+//       reader.read(segment);
+//       const m3u8 = reader.getResult();
+//       for await (const segment of m3u8.segments) {
+//         if (!fs.existsSync(`${outPath}/${segment.url}`)) {
+//           resolve(false);
+//           break;
+//         }
+//       }
+//       resolve(true);
+//     } else {
+//       resolve(false);
+//     }
+//    })
+// }
 
 const downloadConverted = async (fileLocation, outPath) => {
   console.log('download converted file', `${convertedPath}/${fileLocation}/`, `${outPath}/`);
   return new Promise(async (resolve) => {
-    const startDownload = () => {
+    // const startDownload = () => {
       const rclone = spawn('rclone', ['copy', '--progress', '--no-update-modtime', `${convertedPath}/${fileLocation}/`, `${outPath}/`]);
       rclone.stdout.on('data', async (data) => {
         console.log(`rclone download converted stdout: ${data}`);
       });
       rclone.stderr.on('data', async (data) => {
         console.log(`Stderr: ${data}`);
-      });
+      }); 
       rclone.on('close', async (code) => {
         console.log(`download converted file done with code:`, code);
         resolve(true);
       })
-    }
-    const keyPath = `${outPath}/key.vgmk`;
-    const m3u8Path = `${outPath}/128p.m3u8`;
-    if (fs.existsSync(outPath) && fs.existsSync(keyPath) && fs.existsSync(m3u8Path)) {
-      const reader = new M3U8FileParser();
-      const segment = await fs.readFileSync(m3u8Path, { encoding: 'utf-8' });
-      reader.read(segment);
-      const m3u8 = reader.getResult();
-      for (const segment of m3u8.segments) {
-        if (!fs.existsSync(`${outPath}/${segment.url}`)) {
-          startDownload();
-        } else {
-          resolve(true);
-        }
-      }
-    } else {
-      startDownload();
-    }
+    // }
+    // const fileIsFull = await checkFileIsFull(outPath);
+    // console.log('fileIsFull:',fileIsFull);
+      // if (fileIsFull) {
+      //   resolve(true);
+      // } else {
+      //   startDownload();
+      // }
   });
 }
 
@@ -117,11 +129,14 @@ const processFile = async (file: string) => {
     // let fileInfo: any = JSON.parse(jsonString);
     // console.log('old file info', fileInfo);
     const cloudPath = file.replace(/\./g, '\/');
-    const fileDir = `${localTemp}/${file}`; // `${localTemp}/${cloudPath}`;
-    const fileExist = await checkFileExists(cloudPath);
+    const fileDir = `${localTemp}/${file}`; // `${localTemp}/${cloudPath}`; // `${localTemp}/${file}`;
+    // const fileExist = await checkFileExists(cloudPath);
+    const fileExist = await execSync(`rclone lsf '${convertedPath}/${cloudPath}/${quality}p.m3u8'`, {encoding: 'utf8'}) ? true : false; // if video 480p.m3u8 audio 128p.m3u8
+    console.log('file Existtttt:',fileExist);
+    
     if (fileExist) {
       await downloadConverted(cloudPath, fileDir);
-      await execSync(`bash mv-vgmx.sh "${fileDir}"`);
+      // await execSync(`bash mv-vgmx.sh "${fileDir}"`); // for audio only
       const cid:any = await uploadIPFS(fileDir);
       console.warn('cid from ipfs', cid);
       // fileInfo.qm = cid.toString();
@@ -129,8 +144,8 @@ const processFile = async (file: string) => {
       // fileInfo.hash = CryptoJS.AES.encrypt(fileInfo.qm, secretKey).toString();
       // console.log('updated fileInfo', fileInfo);
       if (cid) {
-        await fs.rmdirSync(fileDir, {recursive: true})
-        resolve(true)
+        // await fs.rmdirSync(fileDir, {recursive: true})
+        resolve(cid)
       }
     } else {
       resolve(false);
@@ -154,9 +169,9 @@ const main = async () => {
               const result = await processFile(list[i]);
               console.log('processed files', i);
               if (result) {
-                await fs.appendFileSync(`${prefix}/database/${fileType}-ipfs-count.txt`, `\n${i}`);
+                await fs.appendFileSync(`${prefix}/database/${fileType}-inipfs-count.txt`, `\n${i}|${list[i]}|${result}`);
               } else {
-                await fs.appendFileSync(`${prefix}/database/${fileType}-ipfs-count.txt`, `\n${i}-notfound: ${list[i]}`);
+                await fs.appendFileSync(`${prefix}/database/${fileType}-inipfs-count.txt`, `\n${i}-notfound: ${list[i]}`);
               }
             });
           })();
